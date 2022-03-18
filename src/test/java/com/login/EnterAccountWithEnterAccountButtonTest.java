2b48ac001712185ee153e5c0b7d086772379d3d2
Ekaterina Zarubina <edubovenko@gmail.com>
2022-03-18 21:37:21 +0700

    EnterAccountWithEnterAccountButtonTest final

--- src/test/java/com/login/EnterAccountWithEnterAccountButtonTest.java
+++ src/test/java/com/login/EnterAccountWithEnterAccountButtonTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 
 import java.util.Map;
 
+import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Selenide.page;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -34,10 +35,11 @@ public class EnterAccountWithEnterAccountButtonTest extends BaseTest {
         mainPage.buttonEnterAccount.click();
         loginPage.fillLoginForm(userEmail, userPassword);
 
+        //Подождать пока кнопка появится и убедится что она действительно появилась
+        mainPage.buttonCreateOrder.shouldBe(enabled);
         //Проверку проверить, что отображается кнопка Оформить заказ
         boolean createOrderButtonIsDisplayed = mainPage.isOrderButton();
         System.out.println(createOrderButtonIsDisplayed);
         assertTrue(createOrderButtonIsDisplayed);
-
     }
 }
